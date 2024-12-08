e385e245c83ebf09b63fb7b067c7f07ee32d7c36
MubarratHossain <tahsinezab97@gmail.com>
2024-12-08 10:33:01 +0600

    Hidden the firebase auth keys by creating .env file in root of the project

--- EquiSports/src/firebase/firebase.init.js
+++ EquiSports/src/firebase/firebase.init.js
@@ -1,20 +1,18 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyB6HPbdIt76hP_zJXlT4Ri2WFwLORkr7fY",
-  authDomain: "equisports-d27b8.firebaseapp.com",
-  projectId: "equisports-d27b8",
-  storageBucket: "equisports-d27b8.firebasestorage.app",
-  messagingSenderId: "890492929717",
-  appId: "1:890492929717:web:7899a72dbbb859f9b560fc"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
